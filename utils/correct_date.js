c155ab030e67756d72f8b7b839f30b5c2e4413ee
chrisvobi <christoforidis02@gmail.com>
2024-12-03 21:50:43 +0200

    function that checks if date is given correctly based on app's requirements

---
+++ utils/correct_date.js
@@ -0,0 +1,10 @@
+exports.correct_date = function (date) {
+    return (Array.isArray(date) && date.length === 2
+        && Number.isInteger(date[0]) && Number.isInteger(date[1])
+        && date[0] >= 1 && date[0] <= 12
+        && date[1] >= 1 && (
+            (date[0] === 1 || date[0] === 3 || date[0] === 5 || date[0] === 7 || date[0] === 8 || date[0] === 10 || date[0] === 12) && date[1] <= 31 ||
+            (date[0] === 4 || date[0] === 6 || date[0] === 9 || date[0] === 11) && date[1] <= 30 ||
+            (date[0] === 2 && date[1] <= 28)
+    ));
+};
