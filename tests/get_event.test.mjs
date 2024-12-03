89e76a13223edf03128918d22b5537d7541e5d3f
chrisvobi <christoforidis02@gmail.com>
2024-12-03 22:10:23 +0200

    added one more example in last test to get 100 coverage

--- tests/get_event.test.mjs
+++ tests/get_event.test.mjs
@@ -90,7 +90,7 @@ test("GET users/{user-id}/calendar/{date}/{event-name} returns 400 name isnt str
 // 400 bad request because date in wrong format
 test("GET users/{user-id}/calendar/{date}/{event-name} returns 400 wrong date format", async (t) => {
     const userId = generateRandomID(1, 120);
-    const wrongDates = ["12",12,"asd","aa-aa","aa-12","13-12","2-30"]; // just some examples whole testing for date in utils/correct_date.js
+    const wrongDates = ["12",12,"asd","aa-aa","aa-12","13-12","2-30","0-0"]; // just some examples whole testing for date in utils/correct_date.js
     const choice = generateRandomID(0, wrongDates.length);
     const dateStr = wrongDates[choice];
     let date;
