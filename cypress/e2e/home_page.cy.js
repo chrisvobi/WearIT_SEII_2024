db7882ffe17d1034a0ab19eb92e8a18c8ba7c0c9
chrisvobi <christoforidis02@gmail.com>
2024-12-05 12:54:14 +0200

    update home_page cypress tests

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -3,13 +3,19 @@ describe('Home Page', () => {
 		cy.visit('http://localhost:8080/docs')
 	})
 
-    it('Home Page', () => {
+    it('Check if the page is displayed correctly', () => {
         cy.contains('h2', 'WearIT API')
+    })
+
+    it('Check if I can click the arrows', () => {
         cy.get(".expand-operation").click() // check if i can click the default arrow
         cy.get('div#model-Garment span.model-toggle').click() // check if i can expand the garment schema
         cy.contains('.model', "brand") // check if correct text in it
         cy.get('h4 > svg').click(); // check if i can click the schemas arrow
-        cy.get(".download-url-wrapper").type("asd") // check if i can type in search
+    })
+
+    it ('Check if I can use the search', () => {
+        cy.get(".download-url-input").type("{selectAll}{backspace}testing") // check if i can type in search
         cy.get(".download-url-button").click() // check if i can click the explore button 
     })
 })
