5d53a680c227ffd13bac54d3d4d6d308eb6af253
chrisvobi <christoforidis02@gmail.com>
2024-12-11 19:24:38 +0200

    removed async/awawait from controllers

--- controllers/Calendar.js
+++ controllers/Calendar.js
@@ -4,8 +4,8 @@ var utils = require('../utils/writer.js');
 var Calendar = require('../service/CalendarService');
 
 // GET users/{userId}/calendar
-module.exports.getUserCalendar = async function getUserCalendar (req, res, next, userId) {
-    await Calendar.getUserCalendar(userId)
+module.exports.getUserCalendar = function getUserCalendar (req, res, next, userId) {
+    Calendar.getUserCalendar(userId)
       .then(function (response) {
         utils.writeJson(res, response.body, response.statusCode);
       })
@@ -15,8 +15,8 @@ module.exports.getUserCalendar = async function getUserCalendar (req, res, next,
   };
 
   // POST users/{userId}/calendar
-  module.exports.addUserCalendarEvent = async function addUserCalendarEvent (req, res, next, body, userId) {
-    await Calendar.addUserCalendarEvent(body, userId)
+  module.exports.addUserCalendarEvent = function addUserCalendarEvent (req, res, next, body, userId) {
+    Calendar.addUserCalendarEvent(body, userId)
       .then(function (response) {
         utils.writeJson(res, response, 201);
       })
@@ -26,8 +26,8 @@ module.exports.getUserCalendar = async function getUserCalendar (req, res, next,
   };
 
   // GET users/{userId}/calendar/{date}/{eventName}
-  module.exports.getUserCalendarEvent = async function getUserCalendarEvent (req, res, next, userId, date, eventName) {
-    await Calendar.getUserCalendarEvent(userId, date, eventName)
+  module.exports.getUserCalendarEvent = function getUserCalendarEvent (req, res, next, userId, date, eventName) {
+    Calendar.getUserCalendarEvent(userId, date, eventName)
       .then(function (response) {
         utils.writeJson(res, response);
       })
