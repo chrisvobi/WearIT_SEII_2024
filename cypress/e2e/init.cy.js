807c7a6ab08a19c6be979d9144aac1246c1834a0
chrisvobi <christoforidis02@gmail.com>
2024-12-14 16:26:35 +0200

    fixed minor issues in frontend tests

--- cypress/e2e/init.cy.js
+++ cypress/e2e/init.cy.js
@@ -6,7 +6,7 @@ describe('login', () => {
     it('GET OUTFITS TEST', () => {
         cy.get('#operations-default-getOutfit').click();
         cy.get('.try-out').click();
-        cy.get('input[placeholder="user-id - the id of the user"]').type(`'exampleUserId'`);
+        cy.get('input[placeholder="userId - the id of the user"]').type(`'exampleUserId'`);
         cy.get('input[placeholder="name - the name of the outfit"]').type(`'exampleNameOfTheOutfit'`);
         cy.get(".opblock-control__btn").click(); 
         cy.get(".content-type").should('have.length', 1);
@@ -17,7 +17,7 @@ describe('login', () => {
         cy.get('#operations-default-createOutfit').click();
         cy.get('p').contains("FR7-The user must be able to save outfit");
         cy.get('.try-out').click();
-        cy.get('input[placeholder="user-id - user that creates the outfit"]').type(`'exampleUserId'`);
+        cy.get('input[placeholder="userId - user that creates the outfit"]').type(`'exampleUserId'`);
         cy.get(".content-type").should('have.length', 2);
         cy.get('p').contains("Outfit model");
         cy.get(".examples-select").should('have.length', 1);
