2169e0b20ebb35e179cd2dc276288687653fa193
chrisvobi <christoforidis02@gmail.com>
2024-12-14 17:15:21 +0200

    GET users/{userId}/outfits/{name} tests

--- controllers/Outfit.js
+++ controllers/Outfit.js
@@ -3,11 +3,22 @@
 var utils = require('../utils/writer.js');
 var Outfit = require('../service/OutfitService');
 
-// POST /users/{userId}/outfit
+// POST users/{userId}/outfit
 module.exports.createOutfit = function createOutfit (req, res, next, body, userId) {
   Outfit.createOutfit(body, userId)
     .then(function (response) {
       utils.writeJson(res, response, 201);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response.body, response.statusCode);
+    });
+};
+
+// GET users/{userId}/outfits/{name}
+module.exports.getOutfit = function getOutfit (req, res, next, userId, name) {
+  Outfit.getOutfit(userId, name)
+    .then(function (response) {
+      utils.writeJson(res, response);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
--- service/OutfitService.js
+++ service/OutfitService.js
@@ -18,21 +18,66 @@ exports.createOutfit = function(body,userId) {
   });
 }
 
-/* exports.createOutfit = function(body,userId) {
-    return new Promise(function(resolve, reject) {
-      var examples = {};
-      examples['application/json'] = [];
-      if (userId >= 1 && userId <= 1000) {
-        examples['application/json'].push(body);
-        resolve({
-            statusCode: 201,
-            message: examples[Object.keys(examples)][0]
-        });
-      } else if (userId > 1000) {
+// GET users/{userId}/outfits/{name}
+exports.getOutfit = function(userId,name) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [{
+      "garments" : [ {
+        "size" : "M",
+        "imagePath" : "../images/CameraRoll/PIC01_12_01_2024.jpeg",
+        "name" : "BlackHoodie",
+        "brand" : "Nike"
+      }, {
+        "size" : "M",
+        "imagePath" : "../images/CameraRoll/PIC02_12_01_2024.jpeg",
+        "name" : "GreySweatpants",
+        "brand" : "Nike"
+      }, {
+        "size" : "M",
+        "imagePath" : "../images/CameraRoll/PIC03_12_01_2024.jpeg",
+        "name" : "WhiteShoes",
+        "brand" : "Converse"
+      } ],
+      "name" : "EverydayOutfit"
+  },
+  {
+    "garments" : [ {
+      "size" : "M",
+      "imagePath" : "../images/CameraRoll/PIC04_12_01_2024.jpeg",
+      "name" : "GreyCrewneck",
+      "brand" : "Zara"
+    }, {
+      "size" : "M",
+      "imagePath" : "../images/CameraRoll/PIC05_12_01_2024.jpeg",
+      "name" : "BlackFormalPants",
+      "brand" : "H&M"
+    }, {
+      "size" : "M",
+      "imagePath" : "../images/CameraRoll/PIC06_12_01_2024.jpeg",
+      "name" : "WhiteAirforceShoes",
+      "brand" : "Nike"
+    } ],
+    "name" : "CoffeeDate"
+  }];
+    if (userId > 120) {
+      reject({
+        statusCode: 404,
+        body: "User doesn't exist"
+      })
+    }
+    else if (Object.keys(examples).length > 0) {
+      const outfits = examples[Object.keys(examples)];
+      const index = outfits.findIndex(outfit => outfit.name === name);
+      if (index !== -1) { // outfit with given name exists
+        resolve({body: outfits[index]});
+      }
+      else { // outfit doesn't exist
         reject({
           statusCode: 404,
-          message: "User doesnt exist"
-        });
-      }
-    });
-  } */
+          body: "Outfit with this name doesn't exist"
+        })
+      };
+    }
+  });
+}
