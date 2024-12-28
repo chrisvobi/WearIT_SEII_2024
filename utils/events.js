7e65108cb30f084c56b824af0064fbd1b186761e
chrisvobi <christoforidis02@gmail.com>
2024-12-28 12:24:25 +0200

    removed duplicates for utils/event.js

--- utils/events.js
+++ utils/events.js
@@ -51,28 +51,9 @@ const correctEvent = { // dummy event user wants to add to calendar
   };
   
   const falseEvent2 = { // missing month property
-    "date" : 2,
-    "day" : "Monday",
-    "title" : "Afternoon Coffee",
-    "planndedOutfit" :  {
-      "garments" : [ {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC04_12_01_2024.jpeg",
-        "name" : "GreyCrewneck",
-        "brand" : "Zara"
-      }, {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC05_12_01_2024.jpeg",
-        "name" : "BlackFormalPants",
-        "brand" : "H&M"
-      }, {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC06_12_01_2024.jpeg",
-        "name" : "WhiteAirforceShoes",
-        "brand" : "Nike"
-      } ],
-      "name" : "CoffeeDate"
-    } ,
-  };
+    ...correctEvent,
+    month: undefined
+}
+delete falseEvent2.month
 
   module.exports = {correctEvent, falseEvent, falseEvent2};
