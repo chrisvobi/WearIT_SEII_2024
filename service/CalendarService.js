df1ec05d3f276e75f2fd67f62b1a9a92b24085e0
chrisvobi <christoforidis02@gmail.com>
2024-12-28 16:32:29 +0200

    created helper function for finding events in dummy db to remove duplicates in service

--- service/CalendarService.js
+++ service/CalendarService.js
@@ -1,5 +1,5 @@
 'use strict';
-var {getExamples} = require("../utils/events.js");
+var {getExamples, findEvent} = require("../utils/events.js");
 
 // GET users/{userId}/calendar
 exports.getUserCalendar = function(userId) {
@@ -42,121 +42,24 @@ exports.addUserCalendarEvent = function(body,userId) {
 }
 
 // GET users/{userId}/calendar/{date}/{eventName}
-exports.getUserCalendarEvent = function(userId,date,eventName) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = JSON.parse(JSON.stringify(getExamples));
-    if (userId > 120) {
-      reject({
-        statusCode: 404,
-        body: "User doesn't exist"
-      });
-    }
-    else if (Object.keys(examples).length > 0) { // check that events exist
-      const events = examples[Object.keys(examples)];
-      const eventIndex = events.findIndex(event => event.title === eventName); // find event that matches the eventName
-      if (eventIndex !== -1) { // eventName exists
-        const date_array = date.split("-").map(Number); // make array [MM, DD]
-        const matchedEvent = events[eventIndex];
-        const [givenMonth, givenDay] = date_array;
-        if (givenMonth === matchedEvent.month && givenDay === matchedEvent.date) { // check if the dates match
-          resolve({
-            body: matchedEvent
-          });
-        } else { // dates dont match
-          reject({
-            statusCode: 404,
-            body: "Cannot find event, date and name don't match"
-          })
-        }
-      } else { // eventName doesnt exist
-        reject({
-          statusCode: 404,
-          body: "Event name doesn't exist"
-        })
-      }
-    }
+exports.getUserCalendarEvent = function(userId, date, eventName) {
+  return findEvent(userId, date, eventName).then(({ matchedEvent }) => {
+    return { body: matchedEvent };
   });
-}
+};
 
 // PUT users/{userId}/calendar/{date}/{eventName}
-exports.updateUserCalendarEvent = function(body,userId,date,eventName) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = JSON.parse(JSON.stringify(getExamples));
-    if (userId > 120) {
-      reject({
-        statusCode: 404,
-        body: "User doesn't exist"
-      });
-    }
-    else if (Object.keys(examples).length > 0) { // check that events exist
-      const events = examples[Object.keys(examples)];
-      const eventIndex = events.findIndex(event => event.title === eventName); // find event that matches the eventName
-      if (eventIndex !== -1) { // eventName exists
-        const date_array = date.split("-").map(Number); // make array [MM, DD]
-        const matchedEvent = events[eventIndex];
-        const [givenMonth, givenDay] = date_array;
-        if (givenMonth === matchedEvent.month && givenDay === matchedEvent.date) { // check if the dates match
-          examples[Object.keys(examples)][eventIndex] = body; // update with new event
-          resolve({
-            body: examples[Object.keys(examples)][eventIndex]
-          });
-        } else {
-          reject({ // dates dont match
-            statusCode: 404,
-            body: "Cannot find event, date and name don't match"
-          })
-        }
-      } else { // eventName doesnt exist
-        reject({
-          statusCode: 404,
-          body: "Event name doesn't exist"
-        })
-      }
-    }
+exports.updateUserCalendarEvent = function(body, userId, date, eventName) {
+  return findEvent(userId, date, eventName).then(({ events, eventIndex }) => {
+    events[eventIndex] = body; // Update event
+    return { body: events[eventIndex] };
   });
-}
+};
 
 // DELETE users/{userId}/calendar/{date}/{eventName}
-exports.deleteUserCalendarEvent = function(userId,date,eventName) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = JSON.parse(JSON.stringify(getExamples));
-    if (userId > 120) {
-      reject({
-        statusCode: 404,
-        body: "User doesn't exist"
-      })
-    }
-    else if (Object.keys(examples).length > 0) {
-      var events = examples[Object.keys(examples)];
-      var eventIndex = events.findIndex(event => event.title === eventName); // find eventIndex by name
-      if (eventIndex !== -1) {
-          const date_array = date.split("-").map(Number); // make array [MM, DD]
-          const matchedEvent = events[eventIndex];
-          const [givenMonth, givenDay] = date_array;
-          if (givenMonth === matchedEvent.month && givenDay === matchedEvent.date) { // check if date and eventName match
-              examples[Object.keys(examples)].splice(eventIndex, 1); // remove said event from examples
-              events = examples[Object.keys(examples)];
-              eventIndex = events.findIndex(event => event.title === eventName); // check if event was successfully removed
-                if (eventIndex === -1) {
-                  resolve({
-                    body: "Event deleted successfully"
-                });
-                }    
-        } else { // event and date dont match
-          reject({
-            statusCode: 404,
-            body: "Cannot find event, date and name don't match"
-          })
-        }
-  } else { // eventIndex === -1 event with given title doesnt exist
-    reject({
-      statusCode: 404,
-      body: "Event name doesn't exist"
-    })
-  }
-    }
+exports.deleteUserCalendarEvent = function(userId, date, eventName) {
+  return findEvent(userId, date, eventName).then(({ events, eventIndex }) => {
+    events.splice(eventIndex, 1); // Remove event
+    return { body: "Event deleted successfully" };
   });
-}
+};
--- utils/events.js
+++ utils/events.js
@@ -74,4 +74,42 @@ const getExamples = [ // examples for dummy db to be used in the service
     correctEvent
   ]
 
-module.exports = {correctEvent, falseEvent, falseEvent2, getExamples};
+// Helper function to handle common event lookup logic used in CalendarService
+function findEvent(userId, date, eventName) {
+  return new Promise((resolve, reject) => {
+    const examples = { 'application/json': JSON.parse(JSON.stringify(getExamples)) };
+
+    if (userId > 120) { // check if user exists (~120 users)
+      return reject({
+        statusCode: 404,
+        body: "User doesn't exist"
+      });
+    }
+
+    if (Object.keys(examples).length > 0) { // if we have anything saved
+      const events = examples['application/json'];
+      const eventIndex = events.findIndex(event => event.title === eventName);
+
+      if (eventIndex !== -1) { // event name exists
+        const [givenMonth, givenDay] = date.split("-").map(Number);
+        const matchedEvent = events[eventIndex];
+
+        if (givenMonth === matchedEvent.month && givenDay === matchedEvent.date) {
+          return resolve({ events, eventIndex, matchedEvent }); // date matches
+        } else {
+          return reject({ // date doesn't match
+            statusCode: 404,
+            body: "Cannot find event, date and name don't match"
+          });
+        }
+      } else { // event name doesn't exist
+        return reject({
+          statusCode: 404,
+          body: "Event name doesn't exist"
+        });
+      }
+    }
+  });
+}
+
+module.exports = {correctEvent, falseEvent, falseEvent2, getExamples, findEvent};
