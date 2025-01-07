4a2600f47d36c1c1665dc15d45fc8f8db32d5ece
thomasDodopoulos <154242447+thomasDodopoulos@users.noreply.github.com>
2025-01-07 13:33:09 +0200

    Update OutfitService.js

--- service/OutfitService.js
+++ service/OutfitService.js
@@ -21,7 +21,7 @@ exports.createOutfit = function(body,userId) {
 
 // GET users/{userId}/outfits/{name}
 exports.getOutfit = function(userId,name) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve) {
     var examples = {};
     examples['application/json'] = [
        // Mock data: Predefined outfits
