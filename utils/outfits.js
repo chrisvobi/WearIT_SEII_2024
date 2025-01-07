10a2a2a3e6f9263448f5677b87096519c9a76183
thomasDodopoulos <154242447+thomasDodopoulos@users.noreply.github.com>
2025-01-07 18:33:27 +0200

    Update outfits.js

--- utils/outfits.js
+++ utils/outfits.js
@@ -1,41 +1,35 @@
-// A valid dummy outfit object used for testing purposes
+// Common garments array shared between valid and invalid outfits
+const sharedGarments = [
+  {
+    "size": "M",
+    "imagePath": "../images/CameraRoll/PIC01_12_01_2024.jpeg",
+    "name": "BlackHoodie",
+    "brand": "Nike"
+  },
+  {
+    "size": "M",
+    "imagePath": "../images/CameraRoll/PIC02_12_01_2024.jpeg",
+    "name": "GreySweatpants",
+    "brand": "Nike"
+  },
+  {
+    "size": "M",
+    "imagePath": "../images/CameraRoll/PIC03_12_01_2024.jpeg",
+    "name": "WhiteShoes",
+    "brand": "Converse"
+  }
+];
+
+// Valid outfit object
 const validOutfit = {
-    "garments" : [ { // First garment in the outfit
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC01_12_01_2024.jpeg", // Relative path to garment image
-      "name" : "BlackHoodie",// Name of the garment
-      "brand" : "Nike"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC02_12_01_2024.jpeg",
-      "name" : "GreySweatpants",
-      "brand" : "Nike"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC03_12_01_2024.jpeg",
-      "name" : "WhiteShoes",
-      "brand" : "Converse"
-    } ],
-    "name" : "EverydayOutfit"
+  garments: sharedGarments,
+  name: "EverydayOutfit"
 };
 
-const invalidOutfit = {  // Missing the "name" property to simulate an invalid outfit
-  "garments" : [ {
-    "size" : "M",
-    "imagePath" : "../images/CameraRoll/PIC01_12_01_2024.jpeg",
-    "name" : "BlackHoodie",
-    "brand" : "Nike"
-  }, {
-    "size" : "M",
-    "imagePath" : "../images/CameraRoll/PIC02_12_01_2024.jpeg",
-    "name" : "GreySweatpants",
-    "brand" : "Nike"
-  }, {
-    "size" : "M",
-    "imagePath" : "../images/CameraRoll/PIC03_12_01_2024.jpeg",
-    "name" : "WhiteShoes",
-    "brand" : "Converse"
-  } ]
+// Invalid outfit object, built without the "name" property
+const invalidOutfit = {
+  garments: sharedGarments
 };
+
 // Exporting the objects for use in other files (e.g., tests)
-module.exports = {validOutfit, invalidOutfit};
+module.exports = { validOutfit, invalidOutfit };
