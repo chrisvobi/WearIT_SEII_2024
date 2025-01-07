de573af42426ddb0eb6c9b1bb1939aa52c8beff7
thomasDodopoulos <154242447+thomasDodopoulos@users.noreply.github.com>
2025-01-07 18:37:14 +0200

    Update OutfitService.js

--- service/OutfitService.js
+++ service/OutfitService.js
@@ -2,202 +2,122 @@
 
 const { validOutfit, invalidOutfit } = require('../utils/outfits');
 
+// Shared outfit examples
+const sharedOutfits = [
+  validOutfit, // Reference the valid outfit
+  {
+    "garments": [
+      {
+        "size": "M",
+        "imagePath": "../images/CameraRoll/PIC04_12_01_2024.jpeg",
+        "name": "GreyCrewneck",
+        "brand": "Zara"
+      },
+      {
+        "size": "M",
+        "imagePath": "../images/CameraRoll/PIC05_12_01_2024.jpeg",
+        "name": "BlackFormalPants",
+        "brand": "H&M"
+      },
+      {
+        "size": "M",
+        "imagePath": "../images/CameraRoll/PIC06_12_01_2024.jpeg",
+        "name": "WhiteAirforceShoes",
+        "brand": "Nike"
+      }
+    ],
+    "name": "CoffeeDate"
+  }
+];
+
 // POST /users/{userId}/outfit
-// Function to create an outfit for a specific user
-exports.createOutfit = function(body,userId) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [];
+exports.createOutfit = function (body, userId) {
+  return new Promise(function (resolve, reject) {
+    const examples = { 'application/json': [] };
     examples['application/json'].push(body);
-    if (userId>120) {// Check if the user ID is valid (mock logic)
+
+    if (userId > 120) {
       reject({
         statusCode: 404,
         body: "User doesn't exist"
-      })
-    }
-    else if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)][0]);// Return the first outfit in the examples
+      });
+    } else if (Object.keys(examples).length > 0) {
+      resolve(examples['application/json'][0]); // Return the first outfit
     }
   });
-}
+};
 
 // GET users/{userId}/outfits/{name}
-exports.getOutfit = function(userId,name) {
-  return new Promise(function(resolve,reject) {
-    var examples = {};
-    examples['application/json'] = [
-       // Mock data: Predefined outfits
-       {
-        validOutfit
-  },
-  { // Another outfit example
-    "garments" : [ {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC04_12_01_2024.jpeg",
-      "name" : "GreyCrewneck",
-      "brand" : "Zara"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC05_12_01_2024.jpeg",
-      "name" : "BlackFormalPants",
-      "brand" : "H&M"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC06_12_01_2024.jpeg",
-      "name" : "WhiteAirforceShoes",
-      "brand" : "Nike"
-    } ],
-    "name" : "CoffeeDate"
-  }];
-    if (userId > 120)// User validation
-     {
+exports.getOutfit = function (userId, name) {
+  return new Promise(function (resolve, reject) {
+    const examples = { 'application/json': sharedOutfits };
+
+    if (userId > 120) {
       reject({
         statusCode: 404,
         body: "User doesn't exist"
-      })
-    }
-    else if (Object.keys(examples).length > 0) {
-      const outfits = examples[Object.keys(examples)];
-      const index = outfits.findIndex(outfit => outfit.name === name);// Find the outfit by name
-      if (index !== -1) { // outfit with given name exists
-        resolve({body: outfits[index]});
-      }
-      else { // outfit doesn't exist
+      });
+    } else {
+      const outfits = examples['application/json'];
+      const index = outfits.findIndex(outfit => outfit.name === name);
+
+      if (index !== -1) {
+        resolve({ body: outfits[index] });
+      } else {
         reject({
           statusCode: 404,
           body: "Outfit with this name doesn't exist"
-        })
+        });
       }
     }
   });
-}
+};
 
 // PUT users/{userId}/outfits/{name}
-// Function to update an existing outfit for a user
-exports.updateOutfit = function(body,_userId,name) {
-  return new Promise(function(resolve,reject) {
-    var examples = {};
-    examples['application/json'] = [{
-      "garments" : [ {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC01_12_01_2024.jpeg",
-        "name" : "BlackHoodie",
-        "brand" : "Nike"
-      }, {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC02_12_01_2024.jpeg",
-        "name" : "GreySweatpants",
-        "brand" : "Nike"
-      }, {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC03_12_01_2024.jpeg",
-        "name" : "WhiteShoes",
-        "brand" : "Converse"
-      } ],
-      "name" : "EverydayOutfit"
-  },
-  {  // Another outfit example
-    "garments" : [ {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC04_12_01_2024.jpeg",
-      "name" : "GreyCrewneck",
-      "brand" : "Zara"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC05_12_01_2024.jpeg",
-      "name" : "BlackFormalPants",
-      "brand" : "H&M"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC06_12_01_2024.jpeg",
-      "name" : "WhiteAirforceShoes",
-      "brand" : "Nike"
-    } ],
-    "name" : "CoffeeDate"
-  }];
-    if (Object.keys(examples).length > 0) { // check that outfits exist
-      const outfits = examples[Object.keys(examples)];
+exports.updateOutfit = function (body, _userId, name) {
+  return new Promise(function (resolve, reject) {
+    const examples = { 'application/json': sharedOutfits };
+
+    if (Object.keys(examples).length > 0) {
+      const outfits = examples['application/json'];
       const index = outfits.findIndex(outfit => outfit.name === name);
-      if (index !== -1) { // outfit with given name exists
-        examples[Object.keys(examples)][index] = body
-        resolve({body: examples[Object.keys(examples)][index]});
-      }
-      else { // outfit doesn't exist
+
+      if (index !== -1) {
+        examples['application/json'][index] = body;
+        resolve({ body: examples['application/json'][index] });
+      } else {
         reject({
           statusCode: 404,
           body: "Outfit with this name doesn't exist"
-        })
+        });
       }
     }
   });
-}
+};
 
 // DELETE users/{userId}/outfits/{name}
-// Function to delete a specific outfit
-exports.deleteOutfit = function(userId,name) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [{
-      "garments" : [ {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC01_12_01_2024.jpeg",
-        "name" : "BlackHoodie",
-        "brand" : "Nike"
-      }, {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC02_12_01_2024.jpeg",
-        "name" : "GreySweatpants",
-        "brand" : "Nike"
-      }, {
-        "size" : "M",
-        "imagePath" : "../images/CameraRoll/PIC03_12_01_2024.jpeg",
-        "name" : "WhiteShoes",
-        "brand" : "Converse"
-      } ],
-      "name" : "EverydayOutfit"
-  },
-  {
-    "garments" : [ {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC04_12_01_2024.jpeg",
-      "name" : "GreyCrewneck",
-      "brand" : "Zara"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC05_12_01_2024.jpeg",
-      "name" : "BlackFormalPants",
-      "brand" : "H&M"
-    }, {
-      "size" : "M",
-      "imagePath" : "../images/CameraRoll/PIC06_12_01_2024.jpeg",
-      "name" : "WhiteAirforceShoes",
-      "brand" : "Nike"
-    } ],
-    "name" : "CoffeeDate"
-  }];
-  if (userId > 120) {
-    reject({
-      statusCode: 404,
-      body: "User doesn't exist"
-    })
-  }
-  else if (Object.keys(examples).length > 0) { // check that outfits exist
-    var outfits = examples[Object.keys(examples)];
-    var index = outfits.findIndex(outfit => outfit.name === name);
-    if (index !== -1) { // outfit with given name exists
-      examples[Object.keys(examples)].splice(index, 1);
-      outfits = examples[Object.keys(examples)];
-      index = outfits.findIndex(outfit => outfit.name === name);
-      if (index === -1) { // check that outfit was removed
-        resolve({body: "Outfit deleted successfully"});
-      }
-    }
-    else { // outfit doesn't exist
+exports.deleteOutfit = function (userId, name) {
+  return new Promise(function (resolve, reject) {
+    const examples = { 'application/json': sharedOutfits };
+
+    if (userId > 120) {
       reject({
         statusCode: 404,
-        body: "Outfit with this name doesn't exist"
-      })
+        body: "User doesn't exist"
+      });
+    } else if (Object.keys(examples).length > 0) {
+      let outfits = examples['application/json'];
+      const index = outfits.findIndex(outfit => outfit.name === name);
+
+      if (index !== -1) {
+        outfits.splice(index, 1);
+        resolve({ body: "Outfit deleted successfully" });
+      } else {
+        reject({
+          statusCode: 404,
+          body: "Outfit with this name doesn't exist"
+        });
+      }
     }
-  }
   });
-}
+};
