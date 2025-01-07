ce829c99ab45e05505a4707bf4ba24186800aad1
chrisvobi <christoforidis02@gmail.com>
2025-01-07 21:41:22 +0200

    all outfits in one file

--- service/OutfitService.js
+++ service/OutfitService.js
@@ -1,34 +1,7 @@
 'use strict';
 
-const { validOutfit } = require('../utils/outfits');
+const { sharedOutfits } = require('../utils/outfits');
 
-// Shared outfit examples
-const sharedOutfits = [
-  validOutfit, // Reference the valid outfit
-  {
-    "garments": [
-      {
-        "size": "M",
-        "imagePath": "../images/CameraRoll/PIC04_12_01_2024.jpeg",
-        "name": "GreyCrewneck",
-        "brand": "Zara"
-      },
-      {
-        "size": "M",
-        "imagePath": "../images/CameraRoll/PIC05_12_01_2024.jpeg",
-        "name": "BlackFormalPants",
-        "brand": "H&M"
-      },
-      {
-        "size": "M",
-        "imagePath": "../images/CameraRoll/PIC06_12_01_2024.jpeg",
-        "name": "WhiteAirforceShoes",
-        "brand": "Nike"
-      }
-    ],
-    "name": "CoffeeDate"
-  }
-];
 
 // POST /users/{userId}/outfit
 exports.createOutfit = function (body, userId) {
--- utils/outfits.js
+++ utils/outfits.js
@@ -31,5 +31,33 @@ const invalidOutfit = {
   garments: sharedGarments
 };
 
+// Shared outfit examples
+const sharedOutfits = [
+  validOutfit, // Reference the valid outfit
+  {
+    "garments": [
+      {
+        "size": "M",
+        "imagePath": "../images/CameraRoll/PIC04_12_01_2024.jpeg",
+        "name": "GreyCrewneck",
+        "brand": "Zara"
+      },
+      {
+        "size": "M",
+        "imagePath": "../images/CameraRoll/PIC05_12_01_2024.jpeg",
+        "name": "BlackFormalPants",
+        "brand": "H&M"
+      },
+      {
+        "size": "M",
+        "imagePath": "../images/CameraRoll/PIC06_12_01_2024.jpeg",
+        "name": "WhiteAirforceShoes",
+        "brand": "Nike"
+      }
+    ],
+    "name": "CoffeeDate"
+  }
+];
+
 // Exporting the objects for use in other files (e.g., tests)
-module.exports = { validOutfit, invalidOutfit };
+module.exports = { validOutfit, invalidOutfit, sharedOutfits };
