7d8305393403f6b12c28e5076b7ffae6215cca6b
thomasDodopoulos <154242447+thomasDodopoulos@users.noreply.github.com>
2025-01-07 13:54:08 +0200

    unused variables in controller outfit

--- controllers/Outfit.js
+++ controllers/Outfit.js
@@ -4,45 +4,49 @@ var utils = require('../utils/writer.js');
 var Outfit = require('../service/OutfitService');
 
 // POST users/{userId}/outfit
-module.exports.createOutfit = function createOutfit(req, res, body, userId) {
+module.exports.createOutfit = function createOutfit(_, res,_next, body, userId) {
   Outfit.createOutfit(body, userId)
     .then(function (response) {
       utils.writeJson(res, response, 201);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
+      _next();
     });
 };
 
 // GET users/{userId}/outfits/{name}
-module.exports.getOutfit = function getOutfit(req, res, userId, name) {
+module.exports.getOutfit = function getOutfit(_, res,_next, userId, name) {
   Outfit.getOutfit(userId, name)
     .then(function (response) {
       utils.writeJson(res, response);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
+      _next();
     });
 };
 
 // PUT users/{userId}/outfits/{name}
-module.exports.updateOutfit = function updateOutfit(req, res, body, userId, name) {
+module.exports.updateOutfit = function updateOutfit(_, res,_next, body, userId, name) {
   Outfit.updateOutfit(body, userId, name)
     .then(function (response) {
       utils.writeJson(res, response);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
+      _next();
     });
 };
 
 // DELETE users/{userId}/outfits/{name}
-module.exports.deleteOutfit = function deleteOutfit(req, res, userId, name) {
+module.exports.deleteOutfit = function deleteOutfit(_, res,_next, userId, name) {
   Outfit.deleteOutfit(userId, name)
     .then(function (response) {
       utils.writeJson(res, response);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
+      _next();
     });
 };
