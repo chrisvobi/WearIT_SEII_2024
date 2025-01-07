f018b0dc81a8d9fc860c453d1e33aa573f068cd7
Qedrigord <tasaras200@gmail.com>
2025-01-07 11:27:06 +0200

    fix unused variables

--- controllers/Category.js
+++ controllers/Category.js
@@ -7,35 +7,32 @@ var Category = require('../service/CategoryService');
 // Handles all functions related to categories
 
 // Handles the getCategories function
-module.exports.getCategories = function getCategories (_, res, _next, userId) {
+module.exports.getCategories = function getCategories (_, res, __, userId) {
     Category.getCategories(userId)
       .then(function (response) {
         utils.writeJson(res, response.body, response.statusCode);
       })
       .catch(function (response) {
         utils.writeJson(res, response.body, response.statusCode);
-        _next();
       });
   };
 // Handles the getCategoryGarments function
-module.exports.getCategoryGarments = function getCategoryGarments (_, res, _next, userId, categoryName) {
+module.exports.getCategoryGarments = function getCategoryGarments (_, res, __, userId, categoryName) {
   Category.getCategoryGarments(userId, categoryName)
     .then(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
-      _next();
     });
 };
 // Handles the addGarment function
-module.exports.addGarment = function addGarment (_, res, _next, body, userId, categoryName) {
+module.exports.addGarment = function addGarment (_, res, __, body, userId, categoryName) {
   Category.addGarment(body, userId, categoryName)
     .then(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
-      _next();
     });
 };
--- controllers/Garment.js
+++ controllers/Garment.js
@@ -7,35 +7,32 @@ var Garment = require('../service/GarmentService');
 // Handles all functions related to garments
 
 // Handles the getGarment function
-module.exports.getGarment = function getGarment (_, res, _next, userId, categoryName, name) {
+module.exports.getGarment = function getGarment (_, res, __, userId, categoryName, name) {
     Garment.getGarment(userId, categoryName, name)
       .then(function (response) {
         utils.writeJson(res, response.body, response.statusCode);
       })
       .catch(function (response) {
         utils.writeJson(res, response.body, response.statusCode);
-        _next();
       });
   };
 // Handles the editGarment function
-module.exports.editGarment = function editGarment (_, res, _next, body, userId, categoryName, name) {
+module.exports.editGarment = function editGarment (_, res, __, body, userId, categoryName, name) {
   Garment.editGarment(body, userId, categoryName, name)
     .then(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
-      _next();
     });
   };
 // Handles the deleteGarment function
-module.exports.deleteGarment = function deleteGarment (_, res, _next, userId, categoryName, name) {
+module.exports.deleteGarment = function deleteGarment (_, res, __, userId, categoryName, name) {
   Garment.deleteGarment(userId, categoryName, name)
     .then(function (response) {
       utils.writeJson(res, response);
     })
     .catch(function (response) {
       utils.writeJson(res, response.body, response.statusCode);
-      _next();
     });
 };
